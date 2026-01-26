5b6c3b78d9e8144759eaa21098b8427365720806
DuyViet04 <duyviet130904@gmail.com>
2026-01-26 20:24:53 +0700

    fix: change port

--- client-src/src/ClientMain.java
+++ client-src/src/ClientMain.java
@@ -1,9 +1,9 @@
-﻿import java.net.Socket;
+import java.net.Socket;
 
 public class ClientMain {
     public static void main(String[] args) {
         String pcHost = "10.10.239.176";
-        int port = 139;
+        int port = 5000;
 
         try {
             Socket socket = new Socket(pcHost, port);
--- server-src/src/ServerMain.java
+++ server-src/src/ServerMain.java
@@ -1,12 +1,12 @@
-﻿import java.net.ServerSocket;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServerMain {
-    int port = 139;
     public static void main(String[] args) {
+        int port = 5000;
         System.out.println("Server dang chay...");
         try {
-            ServerSocket serverSocket = new ServerSocket(139);
+            ServerSocket serverSocket = new ServerSocket(port);
             Socket clientSocket = serverSocket.accept();
             System.out.println("Client da ket noi.");
         } catch (Exception e) {
