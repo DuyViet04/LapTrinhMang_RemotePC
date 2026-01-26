64feb702af61a1f9298942a7c22c719d4bd90302
DuyViet04 <duyviet130904@gmail.com>
2026-01-26 20:11:02 +0700

    add: server main class

---
+++ server-src/src/ServerMain.java
@@ -0,0 +1,16 @@
+﻿import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerMain {
+    int port = 139;
+    public static void main(String[] args) {
+        System.out.println("Server dang chay...");
+        try {
+            ServerSocket serverSocket = new ServerSocket(139);
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client da ket noi.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
