91c26677156aaffc7ae82c6df4c9e1f98dfb94ab
DuyViet04 <duyviet130904@gmail.com>
2026-01-26 20:19:24 +0700

    add: client main class

---
+++ client-src/src/ClientMain.java
@@ -0,0 +1,14 @@
+﻿import java.net.Socket;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        String pcHost = "10.10.239.176";
+        int port = 139;
+
+        try {
+            Socket socket = new Socket(pcHost, port);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
